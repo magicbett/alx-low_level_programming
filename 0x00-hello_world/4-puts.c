ca0de2778a7478d7f6bbe71585b01c0b35c5a3fe
magicbett <stanley.kariba@yahoo.com>
2022-06-30 21:11:06 +0300

    NO. 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *main - How to use Puts function
+ *
+ *Return: Always 0 (Success)
+ * */
+int main(void)
+{
+	char phrase[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(phrase);
+	return (0);
+}
