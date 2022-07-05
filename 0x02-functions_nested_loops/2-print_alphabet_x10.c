d838e8d327c7131ea15b96f284f66d45a1b56985
magicbett <stanley.kariba@yahoo.com>
2022-07-05 08:21:01 -0700

    file 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - Begin Here
+*print a-z and A-Z using putchar
+*Return: Value 0 (true)
+**/
+void print_alphabet_x10(void)
+
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{	
+		char ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+	_putchar('\n');
+	}
+}
