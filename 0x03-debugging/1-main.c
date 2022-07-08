66745e3aca44dbbe13ac59258cb5c03317765c89
magicbett <stanley.kariba@yahoo.com>
2022-07-07 21:09:53 -0700

    I hope this does it

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,7 +4,6 @@
 * Return: 0
 */
 int main(void)
-
 {
         int i;
 
--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,7 +9,6 @@
 * Return: void
 */
 void print_remaining_days(int month, int day, int year)
-
 {
 	if ((year % 4 == 0) || ((year % 400 == 0) && !(year % 100 == 0)))
 	{
