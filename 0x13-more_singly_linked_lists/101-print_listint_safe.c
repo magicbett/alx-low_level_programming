0f8bb48f1357c52ab70a0425ceb8b09d1f2b9f1e
magicbett <stanley.kariba@yahoo.com>
2022-08-15 20:55:19 +0300

    Fixed number 101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,27 +1,41 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * print_listint_safe - prints a linked list, safely
- * @head: list of type listint_t to print
- * Return: number of nodes in the list
+ * print_listint_safe - print a linked list only one time
+ * @head: head of LL
+ * Return: counter of nodes & prints an error if the linked list is a circle
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t num = 0;
-	long int diff;
+	const listint_t *slow, *fast, *marker;
+	unsigned int counter = 0, flag = 0;
 
-	while (head)
+	if (head == NULL)
+		return (0);
+	marker = slow = head;
+	fast = head->next;
+	while (head != NULL)
 	{
-		diff = head - head->next;
-		num++;
 		printf("[%p] %d\n", (void *)head, head->n);
-		if (diff > 0)
-			head = head->next;
-		else
+		head = head->next;
+		counter++;
+
+		if (flag == 0 && fast != NULL && fast->next != NULL && slow != NULL)
 		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			if (fast == slow)
+			{
+				flag = 1;
+				slow = marker;
+			}
+			fast = fast->next->next;
+		}
+		if (flag == 1 && slow == head)
+		{
+			printf("-> [%p] %d\n", (void *)head, head->n);
 			break;
 		}
+		slow = slow->next;
 	}
-	return (num);
+	return (counter);
 }
