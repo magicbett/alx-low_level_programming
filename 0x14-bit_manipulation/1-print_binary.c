1e0f54f276e6f518a6e1fd8347cd0a8dcee866e0
magicbett <stanley.kariba@yahoo.com>
2022-08-18 20:54:47 +0300

    number 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary equivalent of a decimal number
+ * @n: number to print
+ */
+void print_binary(unsigned long int n)
+{
+	int b, count = 0;
+	unsigned long int current;
+
+	for (b = 63; b >= 0; b--)
+	{
+		current = n >> b;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
