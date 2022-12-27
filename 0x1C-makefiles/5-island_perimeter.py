e108a2fdb26ad443e799b098c41ffa41fb19e8b8
magicbett <stanley.kariba@yahoo.com>
2022-12-27 07:52:56 +0300

    Makefiles

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+''' Island_Perimeter Module
+'''
+
+
+def island_perimeter(grid):
+    '''
+        Calculates the perimeter of an island that is surrounded by water
+        the Land is represented by 1 and the water is by 0.
+        Aguments:
+            - grid: A matrix filled with 0, 1 representing a map.
+    '''
+    water = 0
+    land = 1
+    perimeter = 0
+
+    for y, level in enumerate(grid):
+        for x, parcel in enumerate(level):
+            if parcel == land:
+                ''' Checking only when hitting land
+                '''
+                if x == 0 or grid[y][x - 1] == water:
+                    ''' Looking west
+                    '''
+                    perimeter += 1
+                if (x + 1) == len(level) or grid[y][x + 1] == water:
+                    perimeter += 1
+                if y == 0 or grid[y - 1][x] == water:
+                    ''' Looking north
+                    '''
+                    perimeter += 1
+                if (y + 1) == len(grid) or grid[y + 1][x] == water:
+                    perimeter += 1
+
+    return perimeter
